2d19534af5f9165492ef41ebd9abb5f55e853c39
Daniel Bayardo <danvc1984@gmail.com>
2019-11-14 17:46:49 -0600

    procesamiento 3er parcial 1

--- MatLab/Procesamiento de Imagenes/3º Parcial/segOtsu.m
+++ MatLab/Procesamiento de Imagenes/3º Parcial/segOtsu.m
@@ -1,16 +1,16 @@
 
-close all;clear all;clc
+close all;
+clear all;
 
-I=imread('lenacolor.png');
+I=imread('obj2_.png');
 figure(1),imshow(I);
 gris= rgb2gray(I);
-
 
 n=imhist(gris); 
 N=sum(n);
 max=0;
 
-%Vector varianzas
+%Vector para guardar varianzas
 varia = [];
 
 for i=1:256
@@ -19,6 +19,7 @@ for i=1:256
 end
 
 for k = 1:255     
+
     %Probabilidad para la class 1
     w0=sum(P(1:k)); 
     %Probabilidad para la class 2
@@ -29,22 +30,43 @@ for k = 1:255
     % media clase 2
     u1=dot([k:255],P(k+1:256))/w1; 
     
-    % La varianza total de la imagen es constante, es la varianza de las
-    % clases, más su complemento.
-    % Calculamos el complemento de la varianza de la imagen y la
-    % maximizamos, que es equivalente a minimizar las varianzas ponderadas
-    % de las clases.
-    sigma=w0*w1*((u1-u0)^2); % Este es el complemento de la varianza de las clases
-    varia(k)= sigma;
-    if sigma >= max; 
-        max=sigma; 
-        % el punto de segmentacion se da por la maxima varianza entre las
-        % clases
-        umbral=k; 
+    vsum = 0;
+    %varianza clase 1
+    for i = 1:k
+     vsum = vsum + ((i-u0)^2)*(n(i));
     end
+    var0 =  vsum/w0;
+    
+    vsum = 0;
+    %varianza clase 2
+    for i = k+1:256
+     vsum = vsum + ((i-u1)^2)*(n(i));
+    end
+    var1 =  vsum/w1;
+    
+    %varianza entre clases
+    sigma = w0*var0+w1*var1;
+   
+    %Calcula el complemento de la varianza 
+    %sigma=w0*w1*((u1-u0)^2); 
+    
+    varia(k)= sigma;
+    
+    % el punto de segmentacion se da por la maxima varianza entre las 
+    %clases  
+%     if sigma >= max; 
+%         max=sigma; 
+%         umbral=k; 
+%     end
 end
 
-
+% el punto de segmentacion se da donde empieza a subir x.
+for x = 1:256
+    if (varia(x)<varia(x+1))
+    umbral = x;
+    break;
+    end
+end
 
 %Muestra el histograma con el umbral resaltado
 figure(2),imhist(gris); 
@@ -55,6 +77,7 @@ line([umbral, umbral], ylim, 'LineWidth', 2 ,'Color',[0.4 0.1 0.5]);
 figure(3),plot(varia);
 hold on;
 line([umbral, umbral], ylim, 'LineWidth', 2 ,'Color',[0.4 0.1 0.5]);
+
 %Muestra resultado en imagen binaria
 imgsegmentada=im2bw(gris,umbral/255); 
 figure(4),imshow(imgsegmentada); 
@@ -63,4 +86,3 @@ figure(4),imshow(imgsegmentada);
 % level = graythresh(I);
 % otsuML = im2bw(level);
 % figure(5),imshow(bw);
-
