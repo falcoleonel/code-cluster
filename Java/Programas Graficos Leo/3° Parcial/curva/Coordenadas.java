e31e42138ebaba438c1488d132650d05e655c2cc
Daniel <daniel@pop-os.localdomain>
2020-01-16 22:32:38 -0600

    Repo Clean up

--- Java/Programas Graficos Leo/3° Parcial/curva/Coordenadas.java
+++
@@ -1,7 +0,0 @@
-package curva;
-
-// Son un conjunto 3 int que determinan la posición del vertice
-public class Coordenadas
-{
-    public int x,y,z;
-}
--- Java/Programas Graficos Leo/3° Parcial/pparalela/Coordenadas.java
+++
@@ -1,7 +0,0 @@
-package pparalela;
-
-// Son un conjunto 3 int que determinan la posición del vertice
-public class Coordenadas
-{
-    public int x,y,z;
-}
