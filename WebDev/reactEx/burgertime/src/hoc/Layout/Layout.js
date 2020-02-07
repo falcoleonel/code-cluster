e6217b61bede62e8add24c031f24094944aaedfa
Daniel <danvc1984@gmail.com>
2020-02-06 18:40:34 -0600

    Burger Time 2.7

--- WebDev/reactEx/burgertime/src/hoc/Layout/Layout.js
+++ WebDev/reactEx/burgertime/src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../Aux/Aux";
+import Aux from "../Auxx/Auxx";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
