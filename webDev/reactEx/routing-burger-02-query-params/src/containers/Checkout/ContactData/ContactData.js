8c5636d391a75314e8e9b6090d2fb3ca09df9353
Daniel <danvc1984@gmail.com>
2020-02-24 15:32:06 -0600

    forms #1

--- webDev/reactEx/routing-burger-02-query-params/src/containers/Checkout/ContactData/ContactData.js
+++ webDev/reactEx/routing-burger-02-query-params/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.css";
 import axios from "./../../../axios-orders";
+import Input from "../../../components/UI/Input/Input";
 class ContactData extends Component {
   state = {
     name: "",
@@ -18,7 +19,7 @@ class ContactData extends Component {
     this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.totalPrice,
+      price: this.props.price,
       customer: {
         name: "Max SchwarzmÃ¼ller",
         address: {
@@ -44,26 +45,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your Name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Your Email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postalCode"
           placeholder="Your Postal Code"
