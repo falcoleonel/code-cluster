47092d792982a4e4aa5f3e17489f8ee004ef42a5
danvc1984 <danvc1984@gmail.com>
2020-04-15 18:40:21 -0500

    Minor Improvments

--- courses/burgerBuilderHooks/src/containers/Checkout/ContactData/ContactData.js
+++ courses/burgerBuilderHooks/src/containers/Checkout/ContactData/ContactData.js
@@ -1,102 +1,103 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-import Button from '../../../components/UI/Button/Button';
-import Spinner from '../../../components/UI/Spinner/Spinner';
-import classes from './ContactData.css';
-import axios from '../../../axios-orders';
-import Input from '../../../components/UI/Input/Input';
-import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import * as actions from '../../../store/actions/index';
-import { updateObject, checkValidity } from '../../../shared/utility';
+import Button from "../../../components/UI/Button/Button";
+import Spinner from "../../../components/UI/Spinner/Spinner";
+import classes from "./ContactData.css";
+import axios from "../../../axios-orders";
+import Input from "../../../components/UI/Input/Input";
+import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from "../../../store/actions/index";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
-const contactData = props => {
+const contactData = (props) => {
   const [orderForm, setOrderForm] = useState({
     name: {
-      elementType: 'input',
+      elementType: "input",
       elementConfig: {
-        type: 'text',
-        placeholder: 'Your Name'
+        type: "text",
+        placeholder: "Your Name",
       },
-      value: '',
+      value: "",
       validation: {
-        required: true
+        required: true,
       },
       valid: false,
-      touched: false
+      touched: false,
     },
     street: {
-      elementType: 'input',
+      elementType: "input",
       elementConfig: {
-        type: 'text',
-        placeholder: 'Street'
+        type: "text",
+        placeholder: "Street",
       },
-      value: '',
+      value: "",
       validation: {
-        required: true
+        required: true,
       },
       valid: false,
-      touched: false
+      touched: false,
     },
     zipCode: {
-      elementType: 'input',
+      elementType: "input",
       elementConfig: {
-        type: 'text',
-        placeholder: 'ZIP Code'
+        type: "text",
+        placeholder: "ZIP Code",
       },
-      value: '',
+      value: "",
       validation: {
         required: true,
         minLength: 5,
         maxLength: 5,
-        isNumeric: true
+        isNumeric: true,
       },
       valid: false,
-      touched: false
+      touched: false,
     },
     country: {
-      elementType: 'input',
+      elementType: "input",
       elementConfig: {
-        type: 'text',
-        placeholder: 'Country'
+        type: "text",
+        placeholder: "Country",
       },
-      value: '',
-      validation: {
-        required: true
-      },
-      valid: false,
-      touched: false
-    },
-    email: {
-      elementType: 'input',
-      elementConfig: {
-        type: 'email',
-        placeholder: 'Your E-Mail'
-      },
-      value: '',
+      value: "",
       validation: {
         required: true,
-        isEmail: true
       },
       valid: false,
-      touched: false
+      touched: false,
+    },
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "Your E-Mail (example user@domain.com)",
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmail: true,
+        validationMessage: "A valid email is required",
+      },
+      valid: false,
+      touched: false,
     },
     deliveryMethod: {
-      elementType: 'select',
+      elementType: "select",
       elementConfig: {
         options: [
-          { value: 'fastest', displayValue: 'Fastest' },
-          { value: 'cheapest', displayValue: 'Cheapest' }
-        ]
+          { value: "fastest", displayValue: "Fastest" },
+          { value: "cheapest", displayValue: "Cheapest" },
+        ],
       },
-      value: 'fastest',
+      value: "fastest",
       validation: {},
-      valid: true
-    }
+      valid: true,
+    },
   });
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const orderHandler = event => {
+  const orderHandler = (event) => {
     event.preventDefault();
 
     const formData = {};
@@ -107,7 +108,7 @@ const contactData = props => {
       ingredients: props.ings,
       price: props.price,
       orderData: formData,
-      userId: props.userId
+      userId: props.userId,
     };
 
     props.onOrderBurger(order, props.token);
@@ -120,10 +121,10 @@ const contactData = props => {
         event.target.value,
         orderForm[inputIdentifier].validation
       ),
-      touched: true
+      touched: true,
     });
     const updatedOrderForm = updateObject(orderForm, {
-      [inputIdentifier]: updatedFormElement
+      [inputIdentifier]: updatedFormElement,
     });
 
     let formIsValid = true;
@@ -138,12 +139,12 @@ const contactData = props => {
   for (let key in orderForm) {
     formElementsArray.push({
       id: key,
-      config: orderForm[key]
+      config: orderForm[key],
     });
   }
   let form = (
     <form onSubmit={orderHandler}>
-      {formElementsArray.map(formElement => (
+      {formElementsArray.map((formElement) => (
         <Input
           key={formElement.id}
           elementType={formElement.config.elementType}
@@ -152,7 +153,7 @@ const contactData = props => {
           invalid={!formElement.config.valid}
           shouldValidate={formElement.config.validation}
           touched={formElement.config.touched}
-          changed={event => inputChangedHandler(event, formElement.id)}
+          changed={(event) => inputChangedHandler(event, formElement.id)}
         />
       ))}
       <Button btnType="Success" disabled={!formIsValid}>
@@ -171,20 +172,20 @@ const contactData = props => {
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
-    userId: state.auth.userId
+    userId: state.auth.userId,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     onOrderBurger: (orderData, token) =>
-      dispatch(actions.purchaseBurger(orderData, token))
+      dispatch(actions.purchaseBurger(orderData, token)),
   };
 };
 
--- courses/burgerBuilderHooks/src/shared/utility.js
+++ courses/burgerBuilderHooks/src/shared/utility.js
@@ -1,37 +1,34 @@
 export const updateObject = (oldObject, updatedProperties) => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    };
+  return {
+    ...oldObject,
+    ...updatedProperties,
+  };
 };
 
-export const checkValidity = ( value, rules ) => {
-    let isValid = true;
-    if ( !rules ) {
-        return true;
-    }
+export const checkValidity = (value, rules) => {
+  let isValid = [];
 
-    if ( rules.required ) {
-        isValid = value.trim() !== '' && isValid;
-    }
+  if (rules.required) {
+    isValid.push(value.trim() !== "");
+  }
 
-    if ( rules.minLength ) {
-        isValid = value.length >= rules.minLength && isValid
-    }
+  if (rules.minLength) {
+    isValid.push(value.length >= rules.minLength);
+  }
 
-    if ( rules.maxLength ) {
-        isValid = value.length <= rules.maxLength && isValid
-    }
+  if (rules.maxLength) {
+    isValid.push(value.length <= rules.maxLength);
+  }
 
-    if ( rules.isEmail ) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test( value ) && isValid
-    }
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid.push(pattern.test(value));
+  }
 
-    if ( rules.isNumeric ) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test( value ) && isValid
-    }
+  if (rules.isNumeric) {
+    const pattern = /^\d+$/;
+    isValid.push(pattern.test(value));
+  }
 
-    return isValid;
-}
+  return isValid.indexOf(false) > -1 ? false : true;
+};
